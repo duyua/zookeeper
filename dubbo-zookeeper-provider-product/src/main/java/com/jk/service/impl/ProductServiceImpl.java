29293fdf881f9ed3964f26298f4daa55e56eafb2
duyua <18653425352@163.com>
2018-07-05 19:26:22 +0800

    commit message

---
+++ dubbo-zookeeper-provider-product/src/main/java/com/jk/service/impl/ProductServiceImpl.java
@@ -0,0 +1,10 @@
+package com.jk.service.impl;
+
+import com.jk.service.IProductService;
+
+public class ProductServiceImpl implements IProductService {
+
+    public String productquery() {
+        return "测通了";
+    }
+}
