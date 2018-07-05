d326902eeb1e8c2e60eb29181334c259667fa383
duyua <18653425352@163.com>
2018-07-05 19:26:35 +0800

    commit message

---
+++ dubbo-zookeeper-provider-indent/src/main/java/com/jk/service/IIndentService.java
@@ -0,0 +1,5 @@
+package com.jk.service;
+
+public interface IIndentService {
+    public String indentlist();
+}
---
+++ dubbo-zookeeper-provider-indent/src/main/java/com/jk/service/impl/IndentServiceImpl.java
@@ -0,0 +1,10 @@
+package com.jk.service.impl;
+
+import com.jk.service.IIndentService;
+
+public class IndentServiceImpl implements IIndentService{
+
+    public String indentlist() {
+        return "订单测通了";
+    }
+}
