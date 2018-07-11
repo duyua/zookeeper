5aa0340cc621028e3abd1113b5093338808db4ca
liruonannan <506514799@qq.com>
2018-07-11 16:02:29 +0800

    提交AttributeService

---
+++ dubbo-zookeeper-customer-demo/src/main/java/com/jk/service/AttributeService.java
@@ -0,0 +1,24 @@
+package com.jk.service;
+
+import com.jk.model.Checkattr;
+import com.jk.model.Tattribute;
+import com.jk.model.Tbrand;
+import com.jk.model.Tclass;
+
+import java.util.List;
+
+public interface AttributeService {
+    List<Tattribute> selectattributeshow();
+
+    List<Tclass> selecttclass();
+
+    void saveattribute(Tattribute attr, String attrname);
+
+    List<Checkattr> selectchecked(String attributeid);
+
+    void deleteall(String ids);
+
+    List<Tbrand> selectbrandshow();
+
+    void saveatbrand(Tbrand brr);
+}
